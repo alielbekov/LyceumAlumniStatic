b673d7b18b51d30615f54d9abf187f9e944ea0ed
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2023-07-10 19:31:19 -0700

    add some people

---
+++ public/scripts/script.js
@@ -0,0 +1,24 @@
+function openModal(imageSrc) {
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imageSrc;
+    $('.bd-example-modal-lg').modal('show'); // Show the modal using Bootstrap jQuery
+
+    // Listen for the popstate event
+    window.addEventListener('popstate', closeModalOnBack);
+    
+    // Update the browser history with a new state
+    history.pushState(null, null, '#modal');
+  }
+
+function closeModalOnBack() {
+const modal = $('.bd-example-modal-lg');
+if (modal.hasClass('show')) {
+    modal.modal('hide'); // Hide the modal using Bootstrap jQuery
+}
+}
+
+// Detect when the modal is closed using Bootstrap events
+$('.bd-example-modal-lg').on('hidden.bs.modal', function () {
+window.removeEventListener('popstate', closeModalOnBack); // Remove the popstate event listener
+history.back(); // Go back to the previous URL
+});
