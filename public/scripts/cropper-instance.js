7986df7b81f87f91b7d6660c381dff125f5b279d
Ali <83210137+alielbekov@users.noreply.github.com>
2023-07-27 19:27:51 +0000

    add name change

--- public/scripts/cropper-instance.js
+++ public/scripts/cropper-instance.js
@@ -1,20 +1,24 @@
-
-const image = document.getElementById('member-image');
-
+const image = document.getElementById("member-image");
 
 const cropper = new Cropper(image, {
-    aspectRatio:1,
-    viewMode: 3
-})
+  aspectRatio: 1,
+  viewMode: 3,
+});
 
-
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutationRecord) {
-        console.log('style changed!');
-    });    
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutationRecord) {
+    console.log("style changed!");
+  });
 });
 const cropperCropBox = document.getElementsByClassName("cropper-crop-box")[0];
-observer.observe(image, { attributes : true, attributeFilter : ['style'] });
+observer.observe(image, { attributes: true, attributeFilter: ["style"] });
 
+function updateLastName() {
+  let lName = document.getElementById("lastNameInput").value;
+  document.getElementById("lastNameOutput").innerHTML = lName;
+}
 
-
+function updateFirstName() {
+  let fName = document.getElementById("firstNameInput").value;
+  document.getElementById("firstNameOutput").innerHTML = fName;
+}
